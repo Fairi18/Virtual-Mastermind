ef2e0abc726a338151fb1b985728e103065c90cf
Farren Kabir <farrenstar10@yahoo.com>
2018-04-16 23:16:25 -0500

    test version

---
+++ VirtualMastermind.py
@@ -0,0 +1,24 @@
+def getGuessFromUser():
+	userGuess = input("Guess: ")
+	return userGuess
+
+def displayFeedback (feedback):
+	print("Feedback: {0} {1} {2} {3}".format(feedback[0], feedback[1],
+		feedback[2], feedback[3]))
+
+def errorMessage():
+	print("Error")
+
+def finalScoreCodebreaker(numOfTurns):
+	print("Score: - {0}".format(numOfTurns))
+
+def main():
+	#Test program....
+	file = open("input.txt", "r")
+	guess = getGuessFromUser()
+	displayFeedback(guess)
+	errorMessage()
+	finalScoreCodebreaker(6)
+
+# test
+main()
