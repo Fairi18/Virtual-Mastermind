e0422f4805f873c12021b387f350ab31ad62621f
Farren Kabir <farrenstar10@yahoo.com>
2018-04-16 23:45:39 -0500

    stripped version

--- VirtualMastermind.py
+++ VirtualMastermind.py
@@ -3,18 +3,25 @@ def getGuessFromUser():
 	return userGuess
 
 def displayFeedback (feedback):
-	print("Feedback: {0} {1} {2} {3}".format(feedback[0], feedback[1],
+	print("Feedback: {0}{1}{2}{3}".format(feedback[0], feedback[1],
 		feedback[2], feedback[3]))
 
 def errorMessage():
 	print("Error")
 
 def finalScoreCodebreaker(numOfTurns):
-	print("Score: - {0}".format(numOfTurns))
+	print("Score: -{0}".format(numOfTurns))
 
 def main():
 	#Test program....
 	file = open("input.txt", "r")
+	CodeList = file.readlines()
+	CleanCodeList = []
+	for x in CodeList:
+		strippedLine = x.strip("\n")
+		CleanCodeList.append(strippedLine)
+	print(CleanCodeList)
+
 	guess = getGuessFromUser()
 	displayFeedback(guess)
 	errorMessage()
